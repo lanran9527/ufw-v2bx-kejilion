b65b70f355dd2a95cdf70d14449ac25be7caffbb
lanran9527 <102012039+lanran9527@users.noreply.github.com>
2025-01-14 22:20:00 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,31 +6,31 @@ apt-get update
 # 安装必需的软件包
 apt install sudo -y
 sudo apt install curl -y
-sudo apt-get install ufw -y
+# sudo apt-get install ufw -y
 sudo apt install nano -y
 
 # 配置防火墙规则
-sudo ufw allow ssh
-sudo ufw allow 53/tcp
-sudo ufw allow 53/udp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 853/tcp
-sudo ufw allow 853/udp
-sudo ufw allow 44443/tcp
-sudo ufw allow 33332/tcp
-sudo ufw allow 35000:36000/udp
-sudo ufw allow 50000/udp
+# sudo ufw allow ssh
+# sudo ufw allow 53/tcp
+# sudo ufw allow 53/udp
+# sudo ufw allow 80/tcp
+# sudo ufw allow 443/tcp
+# sudo ufw allow 853/tcp
+# sudo ufw allow 853/udp
+# sudo ufw allow 44443/tcp
+# sudo ufw allow 33332/tcp
+# sudo ufw allow 35000:36000/udp
+# sudo ufw allow 50000/udp
 
 # 设置iptables规则
-sudo iptables -t nat -A PREROUTING -p udp --dport 35000:36000 -j REDIRECT --to-port 50000
+sudo iptables -t nat -A PREROUTING -p udp --dport 30000:39999 -j REDIRECT --to-port 45678
 
 # 启用UFW防火墙
-sudo ufw --force enable
+# sudo ufw --force enable
 
 # 执行其他安装指令
-wget -N https://raw.githubusercontent.com/wyx2685/V2bX-script/master/install.sh && bash install.sh v0.1.4
-curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
+wget -N https://raw.githubusercontent.com/lanran3698/V2bX-script/master/install.sh && bash install.sh
+# curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
 
 # 修改为上海时区
 sudo timedatectl set-timezone Asia/Shanghai
